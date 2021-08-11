a0e54aa8194b766139dd842bc350ebf8d500d5ad
Jens Reimann <jreimann@redhat.com>
2021-08-11 17:49:20 +0200

    replace influxdb with timescaledb

---
+++ charts/drogue-cloud-examples/scripts/timescaledb/100_init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE temperatures (
+    time TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    device_id VARCHAR(32),
+
+    temperature DOUBLE PRECISION NOT NULL,
+    humidity DOUBLE PRECISION,
+    battery DOUBLE PRECISION,
+
+    lon NUMERIC,
+    lat NUMERIC
+);
+
+SELECT create_hypertable('temperatures', 'time', 'device_id', 2);
